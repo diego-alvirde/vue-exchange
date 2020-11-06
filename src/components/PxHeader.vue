69b4baf3e4655ddcc4a270248e1c50ff65dced39
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-11-05 18:53:30 -0600

    Links y conversión

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -11,7 +11,16 @@
         <div
           class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
         >
-          <div class="text-sm lg:flex-grow"></div>
+          <div class="text-sm lg:flex-grow">
+            <router-link
+              v-for="link in links"
+              :key="link.title"
+              :to="link.to"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              {{ link.title }}
+            </router-link>
+          </div>
         </div>
       </nav>
     </nav>
@@ -24,5 +33,11 @@ import PxIcon from "@/components/PxIcon";
 export default {
   name: "PxHeader",
   components: { PxIcon },
+  props: {
+    links: {
+      type: Array,
+      default: () => [],
+    },
+  },
 };
 </script>
